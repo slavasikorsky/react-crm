1656318df6de028b363ca17a7b39047c8f2d8a63
Slava S <slava.sikorsky@gmal.com>
2023-12-29 20:38:39 +0200

    Chore(useFetch): Hook for future development

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { ClientsRow } from "../types/types";
+
+type FetchResult<T> = {
+	data: T | null;
+	error: { message: string } | null;
+};
+
+type BodyType = Record<string, unknown> | ClientsRow;
+
+const useFetch = <T,>(
+	method: "POST" | "PUT" | "DELETE" | "GET" | "PATCH"
+): FetchResult<T> & {
+	handler: (url: string, body?: BodyType) => void;
+} => {
+	const [data, setData] = useState<T | null>(null);
+	const [error, setError] = useState<FetchResult<T>["error"]>(null);
+
+	const handler = async (URL: string, body?: BodyType) => {
+		await fetch(URL, {
+			method,
+			body: body ? JSON.stringify(body) : null,
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}).then(async (res) => {
+			try {
+				const newData = await res.json();
+				setData(newData);
+			} catch (err) {
+				setError({ message: "An error occurred while data fetching" });
+			}
+		});
+	};
+
+	return { data, error, handler };
+};
+
+export default useFetch;
